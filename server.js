69b3a03aa5b999a577dae998f27a33702899d58a
laraannika <matzoldlara@gmail.com>
2024-11-18 10:52:23 +0100

    test

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.SECRET_EXAMPLE_PORT || 3000;
+const KEY = process.env.SECRET_EXAMPLE_KEY;
+
+app.get('/', (req, res) => {
+    res.send(`Hallo vom Backendserver! Der geheime Schlüssel ist: ${KEY}`);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/`);
+});
