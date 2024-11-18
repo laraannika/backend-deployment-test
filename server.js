5057f1d160d469a7004a099075b2bebbdb34b91e
laraannika <matzoldlara@gmail.com>
2024-11-18 11:03:16 +0100

    test2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.SECRET_EXAMPLE_URI;
 
 const app = express();
 const PORT = process.env.SECRET_EXAMPLE_PORT || 3000;
@@ -7,6 +9,7 @@ const KEY = process.env.SECRET_EXAMPLE_KEY;
 
 app.get('/', (req, res) => {
     res.send(`Hallo vom Backendserver! Der geheime Schlüssel ist: ${KEY}`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}/`);
